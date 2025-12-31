35b620f7b506bbe24db45a6a97009e638651b14e
krishnapriya1 <aadvikaram30@gmail.com>
2025-12-31 23:54:03 +0530

    Project Java

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		int a=2608;
+		String s="Priya";
+		System.out.println(a);
+		System.out.println(s);
+	}
+
+}
